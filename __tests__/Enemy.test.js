d980bf8cb7be4fa869ad72ea92e08b799a7281cc
Lacey <lacey.griffith@g.austincc.edu>
2021-04-15 13:25:30 -0500

    complete enemy object and methods

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -14,3 +14,42 @@ test('create enemy object', () => {
     expect(enemy.agility).toEqual(expect.any(Number))
     expect(enemy.potion).toEqual(expect.any(Object))
 })
+
+test('gets enemy health value', () => {
+    const enemy = new Enemy('Frank', 'sword');
+    expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()))
+})
+
+test('checks if enemy is alive or not', () => {
+    const enemy = new Enemy('Frank', 'sword')
+    expect(enemy.isAlive()).toBeTruthy();
+
+    enemy.health = 0;
+    expect(enemy.isAlive()).toBeFalsy();
+})
+
+test('subtract from enemy health', () => {
+    const enemy = new Enemy('Dave');
+    const oldHealth = enemy.health
+
+    enemy.reduceHealth(5);
+    expect(enemy.health).toBe(oldHealth - 5);
+
+    enemy.reduceHealth(99999)
+    expect(enemy.health).toBe(0);
+})
+
+test('get enemy attack value', () => {
+    const enemy = new Enemy('Dave');
+    enemy.strength = 10;
+
+    expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
+})
+
+test('get enemy description', () => {
+    const enemy = new Enemy('Frank', 'sword')
+
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('Frank'))
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'))
+})
